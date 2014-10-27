c188776964f2091dc7b3167f597a68c34473844f
Hoang Duong <duongtronghoang@gmail.com>
2014-10-26 23:22:14 -0700

    Hoang - Add 3-way Logistic Regression Model. Accuracy improved from 0.65 to 0.737. Add one extra predictor log(SD+1), this boost performance a bit to 0.741 in Logistic Regression Model.

--- Models.R
+++ Models.R
@@ -1,10 +1,15 @@
+library(nnet)
+setwd("~/Dropbox/School/ST215/Lab/lab4/")
 source("DataProcessing.R")
 source("PerformanceMetrics.R")
 
 ########################################################################
 ### 1. Linear Regression
 ########################################################################
-linreg.fit = lm(label ~ NDAI + SD + CORR + DF + CF + BF + AF + AN, 
+train$logSD = log(train$SD+1)
+test$logSD  = log(test$SD +1)
+linreg.fit = lm(label ~ NDAI + SD + CORR + DF + CF + 
+                        BF   + AF + AN   + logSD, 
                 data = train)
 label.hat = predict.lm(linreg.fit, test)
 
@@ -12,9 +17,14 @@ label.hat = predict.lm(linreg.fit, test)
 accuracy(cutOff(label.hat), test$label)
 meanError(cutOff(label.hat), test$label)
 ### Picking the best cut off threshold
+cutOffGridSearch(test$label, label.hat, method = accuracy)
+
 
 #######################################################################
 ### 2. Logistic Regression
 #######################################################################
-
-
+logreg.fit = multinom(label ~ NDAI + SD + CORR + DF + CF +
+                              BF   + AF + AN + logSD,
+                      data = train)
+label.hat2 = as.numeric(predict(logreg.fit, test))
+accuracy(label.hat2, test$label)
