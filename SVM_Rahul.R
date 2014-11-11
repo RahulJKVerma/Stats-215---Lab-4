b830e30db811f0aead8d587314c4350bfb1cb4d3
RAHUL VERMA <administrator@airbears2-10-142-7-152.airbears2.1918.berkeley.edu>
2014-11-11 14:58:56 -0800

    Changes to SVM code

--- SVM_Rahul.R
+++ SVM_Rahul.R
@@ -5,17 +5,18 @@ library('Rcpp')
 library('microbenchmark')
 library('foreach')
 library('doParallel')
+library('pROC')
 
-setwd("~/Dropbox/Fall 14/Stats 215/Stats-215---Lab-4")
+setwd("~/Stats215")
+
 #########################################################################
 ### Hoang's code for creating a training and testing data set ###
 #########################################################################
 
 source("DataProcessing.R")
 source("PerformanceMetrics.R")
-
-
-
+l = getTrainTestBlock(list(image1, image2, image3),k=3, train.pct = 15/27, fix.random = TRUE)
+train = l[[1]]; test = l[[2]]; rm(l);
 
 #########################################################################
 ####### Selecting NDAI, SD and DF as the features used for classification
@@ -26,29 +27,51 @@ source("PerformanceMetrics.R")
 #tuning.parameters <- tune.svm(label~., data = train[,c(3,4,5,7)], gamma = 10^(-6:-1), cost = 10^(-1:1))
 #summary(tuning.parameters)
 
-ncores = 4
+
+#########################################################################
+####### Setting cores
+#########################################################################
+ncores = 30
 registerDoParallel(ncores)
+#########################################################################
+#######
+#########################################################################
 
-start.time1 <- Sys.time()
+#start.time1 <- Sys.time()
+
+
+#########################################################################
+####### Foreach loop
+#########################################################################
+
+
+
+#l = getTrainTestBlock(list(image1, image2, image3),k=3,
+#                      train.pct = 15/27, fix.random = TRUE)
+#train = l[[1]]; test = l[[2]]; rm(l);
+
 #Training the model
-parallel.results <- foreach (.verbose = FALSE, k_value = c(1:15)) %dopar% {
-  
-  l = getTrainTestBlock(list(image1, image2, image3),k=3, 
-                        train.pct = k_value/27, fix.random = TRUE, 
-                        standardize = TRUE)
-  train = l[[1]]; test = l[[2]]; rm(l);
-  
-  svm.model  <- svm(label~., data = train[,c(3,4,5,7)], kernel="polynomial",  degree=3) 
-  #summary(svm.model)
-  #duration.1 <- Sys.time() - start.time1
-  #print(duration.1)
-  
+svm.model  <- svm(label~., data = train[,3:11], kernel="radial",  gamma=.01, cost = .01)
 
-  #Prediction
-  svm.predict <- predict(svm.model, test[,c(4,5,7)])
-  svm.predict.cutoff <- cutOff(svm.predict)
-  #classification.tab <- table(pred = svm.predict.cutoff, true = test[1:1000,3])
-  return_value <- list(accuracy(svm.predict.cutoff, test[,3]),k_value, nrow(svm.model$SV), nrow(test))
-  #classification.tab
-}
+#summary(svm.model)
+#duration.1 <- Sys.time() - start.time1
+#print(duration.1)
+
+#Prediction
+svm.predict <- predict(svm.model, test[,4:11])
+
+#svm.predict.cutoff <- cutOff(svm.predict)
+#classification.tab <- table(pred = svm.predict.cutoff, true = test[1:1000,3])
+
+AUC <- auc(roc(test[,3],svm.predict))
+svm.predict.cutoff <- cutOffGridSearch(test[,3], svm.predict)
+
+#return_value <- list(AUC ,k_value, c_value)
+#classification.tab
+
+
+AUC
+#Area under the curve: 0.9729
+
+#save(svm.predict.cutoff, svm.predict, train, test, file="grid_cutoff.RData")
 
